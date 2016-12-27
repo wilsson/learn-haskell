48e3396efd5d6fd9bb8d96708405d9873a1e8385
wilsonft <wilsonft83@gmail.com>
2016-12-27 13:18:34 -0500

    feat(operadores):agregando operadores

---
+++ 01-operators/operators.hs
@@ -0,0 +1,50 @@
+-- Sum
+7+3
+
+-- Subtraction
+7-3
+
+-- Multiplication
+7*3
+
+-- Division
+7/3
+
+-- Whole quotient - division
+7`div`3
+
+-- Residue - division
+7`mod`3
+
+-- operator and
+True && False
+
+-- operator or
+True || False
+
+-- equal
+7 == 2
+
+-- Different
+7/=3
+
+-- major
+7>3
+
+-- major equal
+7>=3
+
+-- minor
+7<3
+
+-- minor equal
+7<=3
+
+-- negation
+not False
+
+-- Groupers
+(7*3)+(-3)
+
+-- Concatenation
+"hello "++"world!"
